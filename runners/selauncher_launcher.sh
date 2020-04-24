cb83e3f1085c16811e4618fe8f0cc908b5160547
rockerbacon <vitor@librem.one>
2020-04-24 20:35:51 -0300

    Updated selauncher script

--- runners/selauncher_launcher.sh
+++ runners/selauncher_launcher.sh
@@ -2,18 +2,11 @@
 
 EXECUTABLE="SkyrimSELauncher.exe"
 SKYRIM_COMPAT_DATA="$HOME/.steam/steam/steamapps/compatdata/489830"
-
-if [ "$SKYRIM_PROTON_BINARY" == "" ]; then
-    SOURCE_FILE_PATH=$(dirname "$0")
-	SKSE_INSTALL_PATH=$(realpath "$SOURCE_FILE_PATH/..")
-    SKYRIM_PROTON_BINARY="$SKSE_INSTALL_PATH/proton_patch/skyrim-proton/proton"
-fi
-
+SKYRIM_PROTON_BINARY="$HOME/.steam/steam/steamapps/common/Proton 4.11/proton"
 SKYRIM_INSTALL_FOLDER="$HOME/.steam/steam/steamapps/common/Skyrim Special Edition"
-
 
 if [ "$EXECUTABLE" != "" ]; then
     echo "Launching $EXECUTABLE"
     export STEAM_COMPAT_DATA_PATH=$SKYRIM_COMPAT_DATA
-    $SKYRIM_PROTON_BINARY run "$EXECUTABLE"
+    "$SKYRIM_PROTON_BINARY" run "$EXECUTABLE"
 fi
