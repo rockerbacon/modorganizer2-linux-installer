1659cc985d9192a1bc777d45e250b4700e3e7d13
rockerbacon <vitor@librem.one>
2020-05-10 01:35:47 -0300

    Fixed errors in Fallout 3 game info

--- gamesinfo/fallout3.sh
+++ gamesinfo/fallout3.sh
@@ -2,8 +2,8 @@
 fo3_possible_appids=(22300 22370)
 
 for fo3_appid in "${fo3_possible_appids[@]}"; do
-	fo3_library=$("$CACHE/utils/find-library-for-appid.sh $fo3_appid")
-	if [ -d "$fo3_library" ];
+	fo3_library=$("$CACHE/utils/find-library-for-appid.sh" $fo3_appid)
+	if [ -d "$fo3_library" ]; then
 		break
 	fi
 done
@@ -13,7 +13,7 @@ if [ "$fo3_appid" == "22300" ]; then
 else
 	game_steam_subdirectory="Fallout 3 goty"
 fi
-game_appid=$appid
+game_appid=$fo3_appid
 game_launcher_options="--restart-pulse --protonver 5.*"
 game_protontricks="d3dcompiler_43 d3dx9"
 
