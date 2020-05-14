1257a6452e2cacf9b7a82fef10826d1bfdba8949
rockerbacon <vitor@librem.one>
2020-05-14 11:37:08 -0300

    Fixed installation of Russian version of New Vegas

--- gamesinfo/newvegas.sh
+++ gamesinfo/newvegas.sh
@@ -1,5 +1,16 @@
+# global version and russion version are different games on steam
+newvegas_possible_appids=(22380 22490)
+
+for newvegas_appid in "${newvegas_possible_appids[@]}"; do
+	newvegas_library=$("$CACHE/utils/find-library-for-appid.sh" $newvegas_appid)
+	if [ -d "$newvegas_library" ]; then
+		steam_library="$newvegas_library"
+		break
+	fi
+done
+
 game_steam_subdirectory="Fallout New Vegas"
-game_appid=22380
+game_appid=$newvegas_appid
 game_proton_options="--protonver 5.*"
 game_wine_options=""
 game_protontricks="d3dcompiler_43 d3dx9"
