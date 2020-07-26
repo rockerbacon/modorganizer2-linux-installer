946fc94a00f5d14dda70ea5b6202779c207624f3
Fabian Würfl <bafdyce@tuta.io>
2020-07-26 17:32:44 +0200

    Search all steam installations for game

--- utils/find-library-for-appid.sh
+++ utils/find-library-for-appid.sh
@@ -12,40 +12,44 @@ for steam_install in "${steam_install_candidates[@]}"; do
 	echo "Searching for Steam in '$steam_install'" >&2
 	if [ -d "$steam_install" ]; then
 		echo "Found Steam" >&2
-		break
+
+        restore_ifs=$IFS
+        IFS=$'\n'
+            main_library="$steam_install"
+            if [ ! -d "$main_library/steamapps" ]; then
+                main_library="$steam_install/steam"
+            fi
+
+            steam_libraries=("$main_library")
+            steam_libraries+=($( \
+                grep -oE '/[^"]+' "$main_library/steamapps/libraryfolders.vdf" \
+            ))
+        IFS=$restore_ifs
+
+        for libdir in "${steam_libraries[@]}"; do
+            echo "Searching for game in library '$libdir'" >&2
+            if [ -d "$libdir/steamapps/compatdata/$appid" ]; then
+                echo "Found game" >&2
+                break
+            fi
+        done
+
+        if [ ! -d "$libdir/steamapps/compatdata/$appid" ]; then
+            echo "ERROR: could not find game with APPID '$appid'" >&2
+            continue
+        fi
+
+        # we found the game, so we print the libdir and exit
+        echo "$libdir"
+        exit 0
 	fi
 done
+
 if [ ! -d "$steam_install" ]; then
 	msg="could not find Steam"
 	echo "ERROR: $msg" >&2
 	exit 1
 fi
 
-restore_ifs=$IFS
-IFS=$'\n'
-	main_library="$steam_install"
-	if [ ! -d "$main_library/steamapps" ]; then
-		main_library="$steam_install/steam"
-	fi
-
-	steam_libraries=("$main_library")
-	steam_libraries+=($( \
-		grep -oE '/[^"]+' "$main_library/steamapps/libraryfolders.vdf" \
-	))
-IFS=$restore_ifs
-
-for libdir in "${steam_libraries[@]}"; do
-	echo "Searching for game in library '$libdir'" >&2
-	if [ -d "$libdir/steamapps/compatdata/$appid" ]; then
-		echo "Found game" >&2
-		break
-	fi
-done
-
-if [ ! -d "$libdir/steamapps/compatdata/$appid" ]; then
-	echo "ERROR: could not find game with APPID '$appid'" >&2
-	exit 1
-fi
-
-echo "$libdir"
-
+# If we reach this point, we couldnt find the game
+exit 1
