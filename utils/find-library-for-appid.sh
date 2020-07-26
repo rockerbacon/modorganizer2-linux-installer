f2aebe5afc7da997cc982fdffe82b9cc5045ad8d
rockerbacon <vitor@librem.one>
2020-07-26 15:10:06 -0300

    Removed unecessary code

--- utils/find-library-for-appid.sh
+++ utils/find-library-for-appid.sh
@@ -30,26 +30,13 @@ for steam_install in "${steam_install_candidates[@]}"; do
 			echo "Searching for game in library '$libdir'" >&2
 			if [ -d "$libdir/steamapps/compatdata/$appid" ]; then
 				echo "Found game" >&2
-				break
+				echo "$libdir"
+				exit 0
 			fi
 		done
-
-		if [ ! -d "$libdir/steamapps/compatdata/$appid" ]; then
-			echo "ERROR: could not find game with APPID '$appid'" >&2
-			continue
-		fi
-
-		# we found the game, so we print the libdir and exit
-		echo "$libdir"
-		exit 0
 	fi
 done
 
-if [ ! -d "$steam_install" ]; then
-	msg="could not find Steam"
-	echo "ERROR: $msg" >&2
-	exit 1
-fi
-
-# If we reach this point, we couldnt find the game
+echo "ERROR: could not find game with APPID '$appid'" >&2
 exit 1
+
