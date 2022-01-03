eefb4138fae578a3c98d465fdc0ad19dab00f879
Vitor Santos <vitor@librem.one>
2022-01-03 19:11:51 -0300

    Fix download progress bar for curl backend

--- utils/download.sh
+++ utils/download.sh
@@ -3,15 +3,22 @@
 # usage: download.sh url
 #   downloads resource at  url  to the current working directory.
 
-echoerr() {
-	echo "download.sh:" "$@" >&2
+function log_error() {
+	echo "ERROR:" "$@" >&2
 }
 
+function log_info() {
+	echo "INFO:" "$@"
+}
 
 url="$1"
+out_file="$2"
 
-if [ "$url" == "" ]; then
-	echoerr "must specify source url"
+if [ -z "$url" ]; then
+	log_error "must specify source url"
+	exit 1
+elif [ -z "$out_file" ]; then
+	log_error "must specify output file location"
 	exit 1
 fi
 
@@ -20,42 +27,41 @@ fi
 # doesn't overwrite files by default, better progress bar, downloads resources that
 # don't have a name e.g. https://example.com/.
 if [ -n "$DOWNLOAD_BACKEND" ]; then
-	use="$DOWNLOAD_BACKEND"
+	download_backend="$DOWNLOAD_BACKEND"
 elif command -v wget >& /dev/null; then
-	use="wget"
+	log_info "using wget backend"
+	download_backend="wget"
 elif command -v curl >& /dev/null; then
-	use="curl"
+	log_info "using curl backend"
+	download_backend="curl"
 else
-	echoerr "either wget or curl must be installed on this system"
+	log_error "either wget or curl must be installed on this system"
 	exit 1
 fi
 
+log_info "fetching remote resource at '$1'"
 
-echo "download.sh: fetching remote resource at '$1'"
+progress_text="Downloading '$url' into '$out_file'"
 
-case "$use" in
+case "$download_backend" in
 	"wget")
-		wget "$url" --progress=dot --no-verbose --show-progress 2>&1 \
+		wget "$url" --progress=dot --no-verbose --show-progress -O "$out_file" 2>&1 \
 			| grep --line-buffered --color=never -oE '[0-9]+%' \
-			| zenity --progress --auto-kill --auto-close --text="Downloading '${url##*/}'"
-		if [ "$?" != "0" ]; then
-			echoerr "could not fetch '$url'"
-			exit 1
-		fi
+			| zenity --progress --auto-kill --auto-close --text="$progress_text"
 		;;
+
 	"curl")
-		file_name=${url##*/}
 		redirected_url=$(curl -ILs -o /dev/null -w '%{url_effective}' "$url")
 
-		curl "$redirected_url" -o "$file_name" -# 2>&1 \
+		curl "$redirected_url" -o "$out_file" -# 2>&1 \
 			| tr '\r' '\n' \
-			| grep --line-buffered --color=never -oE '[0-9]+,[0-9]+%' \
-			| zenity --progress --auto-kill --auto-close --text="Downloading '${url##*/}'"
-
-		if [ "$?" != "0" ]; then
-			echoerr "could not fetch '$url'"
-			exit 1
-		fi
+			| grep --line-buffered --color=never -oE '[0-9]+\.[0-9]+%' \
+			| zenity --progress --auto-kill --auto-close --text="$progress_text"
 		;;
 esac
+
+if [ "$?" != "0" ]; then
+	log_error "could not fetch '$url'"
+	exit 1
+fi
 
