2f1c88025272751a006840ebefac183876aa9c7b
Vitor Santos <vitor@librem.one>
2022-01-03 22:37:33 -0300

    Fix variable names in extract util and add progress indicator to 7z extraction

--- utils/extract.sh
+++ utils/extract.sh
@@ -9,18 +9,25 @@
 #   - zip
 #   - 7z
 
-echoerr() {
-	echo "extract.sh:" "$@" >&2
+log_error() {
+	echo "ERROR:" "$@" >&2
 }
 
+compressed_file=$1
+destination_dir=$2
 
 # Preliminary checks
-if ! [ -f "$1" ]; then
-	echoerr "'$1' does not exist, aborting"
+if ! [ -f "$compressed_file" ]; then
+	log_error "'$compressed_file' does not exist, aborting"
 	exit 1
 fi
 
-extension="${1#*.}"
+if [ -n "$(ls -A "$destination_dir/")" ]; then
+	log_error "'$destination_dir' is a non-empty directory, aborting"
+	exit 1
+fi
+
+extension="${compressed_file##*.}"
 
 case "$extension" in
 	"zip")
@@ -33,55 +40,54 @@ case "$extension" in
 		cmds_needed=( "7z" )
 		;;
 	*)
-		echoerr "cannot extract '$1' - unsupported archive type"
+		log_error "cannot extract '$compressed_file' - unsupported archive type"
 		exit 1
 		;;
 esac
 
 for cmd in "${cmds_needed[@]}"; do
 	if ! command -v "$cmd" >& /dev/null; then
-		echoerr "command '$cmd' not found but is required for file '$1', aborting"
+		log_error "command '$cmd' not found but is required for file '$compressed_file', aborting"
 		exit 1
 	fi
 done
 
 
-# Now we can begin the extraction
-extractdir="$2"
-if [ -d "$extractdir" ]; then
-	echoerr "'$extractdir' already exists, aborting"
-	exit 1
-fi
-
 # Create dir ready for extraction
-mkdir -p "$extractdir"
-
+mkdir -p "$destination_dir"
 
 # Quick function to leave things as they were found in case we mess up
 cleanup() {
-	echoerr "could not extract $1"
-	rm -rf "$2"
+	log_error "could not extract '$compressed_file'"
+	rm -rf "$destination_dir"
 }
 
+progress_text="Extracting '${compressed_file##*/}'"
 
-echo "extract.sh: extracting '$1' to '$extractdir'"
+echo "INFO: extracting '$compressed_file' to '$destination_dir'"
 
 case "$extension" in
 	"zip")
-		if ! unzip "$1" -d "$extractdir" 1> /dev/null; then
-			cleanup "$1" "$extractdir"
+		if ! unzip "$compressed_file" -d "$destination_dir" 1> /dev/null; then
+			cleanup
 			exit 1
 		fi
 		;;
 	"tar.gz")
-		if ! tar -xvzf "$1" --directory "$extractdir" 1> /dev/null; then
-			cleanup "$1" "$extractdir"
+		if ! tar -xvzf "$compressed_file" --directory "$destination_dir" 1> /dev/null; then
+			cleanup
 			exit 1
 		fi
 		;;
 	"7z")
-		if ! 7z x -o"$extractdir" "$1" 1> /dev/null; then
-			cleanup "$1" "$extractdir"
+		7z x -o"$destination_dir" "$compressed_file" -bsp1 -bso0 \
+			| stdbuf -o128 tr -d '\b' \
+			| stdbuf -oL tr -s ' ' '\n' \
+			| grep --line-buffered --color=never -oE '[0-9]+%' \
+			| zenity --progress --auto-kill --auto-close --text="$progress_text"
+
+		if [ "$?" != "0" ]; then
+			cleanup
 			exit 1
 		fi
 		;;
