f8a09a283d53061467d24855f208a12eb6b9f79c
Vitor Santos <vitor@librem.one>
2022-01-06 18:03:59 -0300

    Added more logs to redirector setup and reduced code duplication

--- step/install_steam_redirector.sh
+++ step/install_steam_redirector.sh
@@ -3,14 +3,21 @@
 mkdir -p "$game_installation/modorganizer2"
 
 installdir_windowspath="Z:$(tr '/' '\\' <<< "$install_dir")"
+mo2_executable_windowspath="$installdir_windowspath\\modorganizer2\\ModOrganizer.exe"
 
-echo "$installdir_windowspath\\modorganizer2\\ModOrganizer.exe" > "$game_installation/modorganizer2/instance_path.txt"
+mo2_executable_path_config="$game_installation/modorganizer2/instance_path.txt"
+
+log_info "configuring mo2 executable path '$mo2_executable_windowspath' in '$mo2_executable_path_config'"
+echo "$mo2_executable_windowspath" > "$mo2_executable_path_config"
 
 original_game_executable="$game_installation/_$game_executable"
 
+full_game_executable_path="$game_installation/$game_executable"
 if [ ! -f "$original_game_executable" ]; then
-	mv "$game_installation/$game_executable" "$original_game_executable"
+	log_info "backing up original executable '$full_game_executable_path' in '$original_game_executable'"
+	mv "$full_game_executable_path" "$original_game_executable"
 fi
 
-cp -f "$redirector/main.exe" "$game_installation/$game_executable"
+	log_info "setting up redirector in '$full_game_executable_path'"
+cp -f "$redirector/main.exe" "$full_game_executable_path"
 
