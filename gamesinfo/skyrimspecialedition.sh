35110dbb2762f9ea003ea74df1ab150ce7e677c4
Lithium/Helium <41258148+LithRakoon@users.noreply.github.com>
2022-01-07 19:45:39 +0100

    Update SKSE64

--- gamesinfo/skyrimspecialedition.sh
+++ gamesinfo/skyrimspecialedition.sh
@@ -6,10 +6,10 @@ game_proton_options="--native 'xaudio2_7' -e 'PULSE_LATENCY_MSEC=90'"
 game_wine_options="--native 'xaudio2_7'"
 game_protontricks=""
 game_winetricks=""
-game_scriptextender_url="https://skse.silverlock.org/beta/skse64_2_01_04.7z"
+game_scriptextender_url="https://skse.silverlock.org/beta/skse64_2_01_05.7z"
 game_scriptextender_files=( \
 	"skse64_2_01_04/Data" \
-	"skse64_2_01_04/skse64_1_6_342.dll" \
+	"skse64_2_01_04/skse64_1_6_353.dll" \
 	"skse64_2_01_04/skse64_loader.exe" \
 	"skse64_2_01_04/skse64_steam_loader.dll" \
 )
