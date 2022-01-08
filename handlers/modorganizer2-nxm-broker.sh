15846fec7a0f12f57c64f18a70858084630c5ba8
Vitor Santos <vitor@librem.one>
2022-01-08 10:40:34 -0300

    Added functionality for downloading mods after mo2 is already running

--- handlers/modorganizer2-nxm-broker.sh
+++ handlers/modorganizer2-nxm-broker.sh
@@ -22,7 +22,25 @@ if [ ! -d "$instance_dir" ]; then
 	exit 1
 fi
 
+instance_dir_windowspath="Z:$(sed 's/\//\\\\/g' <<<$instance_dir)"
+pgrep -f "$instance_dir_windowspath\\\\modorganizer2\\\\ModOrganizer.exe"
+process_search_status=$?
+
 game_appid=$(cat "$instance_dir/appid.txt")
 
-steam -applaunch "$game_appid" "$nxm_link"
+if [ "$process_search_status" == "0" ]; then
+	echo "INFO: sending download '$nxm_link' to running Mod Organizer 2 instance"
+	download_start_output=$(WINEESYNC=1 WINEFSYNC=1 protontricks-launch --appid "$game_appid" "$instance_dir/modorganizer2/nxmhandler.exe" "$nxm_link")
+	download_start_status=$?
+else
+	echo "INFO: starting Mod Organizer 2 to download '$nxm_link'"
+	download_start_output=$(steam -applaunch "$game_appid" "$nxm_link")
+	download_start_status=$?
+fi
+
+if [ "$download_start_status" != "0" ]; then
+	zenity --ok-label=Exit --ellipsize --error --text \
+		"Failed to start download:\n\n$download_start_output"
+	exit 1
+fi
 
