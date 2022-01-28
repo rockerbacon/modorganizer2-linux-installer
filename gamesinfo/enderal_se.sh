9cf2d667c8ba5ed67dc507523dfcd8a9c1e81552
jake770321 <84999457+jake770321@users.noreply.github.com>
2022-01-28 03:40:50 +0000

    Update enderal_se.sh

--- gamesinfo/enderal_se.sh
+++ gamesinfo/enderal_se.sh
@@ -1,6 +1,6 @@
 game_steam_subdirectory="Enderal Special Edition"
 game_nexusid="enderalspecialedition"
-game_appid=976620
+game_appid=933480
 game_executable="Enderal Launcher.exe"
 game_protontricks=("xaudio2_7=native")
 game_scriptextender_url="https://skse.silverlock.org/beta/skse64_2_01_05.7z"
