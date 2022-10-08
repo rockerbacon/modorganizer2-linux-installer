9798d14acdedc7404c36662b2d24cea23e20f1eb
Morogwen <71471326+Morogwen@users.noreply.github.com>
2022-10-08 11:26:47 -0500

    Update skyrimspecialedition.sh
    Update SKSE version to work with latest SE/AE version.

--- gamesinfo/skyrimspecialedition.sh
+++ gamesinfo/skyrimspecialedition.sh
@@ -3,11 +3,10 @@ game_nexusid="skyrimspecialedition"
 game_appid=489830
 game_executable="SkyrimSELauncher.exe"
 game_protontricks=("xaudio2_7=native")
-game_scriptextender_url="https://skse.silverlock.org/beta/skse64_2_01_05.7z"
+game_scriptextender_url="https://skse.silverlock.org/beta/skse64_2_02_03.7z"
 game_scriptextender_files=( \
-	"skse64_2_01_05/Data" \
-	"skse64_2_01_05/skse64_1_6_353.dll" \
-	"skse64_2_01_05/skse64_loader.exe" \
-	"skse64_2_01_05/skse64_steam_loader.dll" \
+	"skse64_2_02_03/Data" \
+	"skse64_2_02_03/skse64_1_6_640.dll" \
+	"skse64_2_02_03/skse64_loader.exe" \
 )
 
