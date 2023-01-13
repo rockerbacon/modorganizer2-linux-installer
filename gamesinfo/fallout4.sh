2d5106dcb49a9e560338fd3c740520b440a22f66
lehjr <lehjr1@gmail.com>
2023-01-13 08:57:35 -0500

    Update fallout4.sh
    update FOSE

--- gamesinfo/fallout4.sh
+++ gamesinfo/fallout4.sh
@@ -3,6 +3,6 @@ game_nexusid="fallout4"
 game_appid=377160
 game_executable="Fallout4Launcher.exe"
 game_protontricks=("xaudio2_7=native")
-game_scriptextender_url="https://f4se.silverlock.org/beta/f4se_0_06_21.7z"
+game_scriptextender_url="https://f4se.silverlock.org/beta/f4se_0_06_23.7z"
 game_scriptextender_files="*"
 
