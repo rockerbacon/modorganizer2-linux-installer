6e0e5f99a2856c553f7165634fe9e10c12a9dca1
rockerbacon <vitor@librem.one>
2023-07-20 15:50:36 -0300

    Ensure applications directory exists before copying files to it

--- step/install_nxm_handler.sh
+++ step/install_nxm_handler.sh
@@ -4,8 +4,10 @@ log_info "installing nxm link broker in '$shared'"
 cp "$handlers/modorganizer2-nxm-broker.sh" "$shared"
 chmod +x "$shared/modorganizer2-nxm-broker.sh"
 
-log_info "installing nxm link handler in '$HOME/.local/share/applications/'"
-cp "$handlers/modorganizer2-nxm-handler.desktop" "$HOME/.local/share/applications/"
+app_dir="$HOME/.local/share/applications"
+log_info "installing nxm link handler in '$app_dir/'"
+mkdir -p "$app_dir"
+cp "$handlers/modorganizer2-nxm-handler.desktop" "$app_dir/"
 
 echo "$game_appid" > "$install_dir/appid.txt"
 
