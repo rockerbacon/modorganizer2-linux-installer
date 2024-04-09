0a1fb3d763cfd37f8bb497c038303905a87d0159
kurumushi <kurumushi@users.noreply.github.com>
2024-04-09 17:36:51 -0400

    Fix whitepace

--- workarounds/stardewvalley.sh
+++ workarounds/stardewvalley.sh
@@ -2,13 +2,13 @@
 
 # Install SMAPI
 if [ -d "$extracted_scriptextender" ]; then
-		log_info "installing SMAPI in '$game_installation'"
+	log_info "installing SMAPI in '$game_installation'"
 
-        # This command is ugly, but necessary 
-        # Needs to run through wineconsole in order to work with .NET's System.Console
+	# This command is ugly, but necessary 
+	# Needs to run through wineconsole in order to work with .NET's System.Console
 
-        "$utils/protontricks.sh" run-command "$game_appid" "wineconsole '${extracted_scriptextender}/SMAPI ${game_smapi_version} installer/internal/windows/SMAPI.Installer.exe' --install --no-prompt --game-path 'Z:\mnt\lingames\SteamLibrary\steamapps\common\Stardew Valley'" \
-		    | "$dialog" loading "Installing SMAPI\nThis may take a while"
+	"$utils/protontricks.sh" run-command "$game_appid" "wineconsole '${extracted_scriptextender}/SMAPI ${game_smapi_version} installer/internal/windows/SMAPI.Installer.exe' --install --no-prompt --game-path 'Z:\mnt\lingames\SteamLibrary\steamapps\common\Stardew Valley'" \
+		| "$dialog" loading "Installing SMAPI\nThis may take a while"
 
 	if [ "$?" != "0" ]; then
 		"$dialog" errorbox \
