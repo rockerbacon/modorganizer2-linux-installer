25262afed28d1d15505066b7d0ee437675621e36
Kris Scott <kris@syntosis.net>
2024-04-12 21:34:53 -0400

    Make installer work on other systems than mine.

--- workarounds/stardewvalley.sh
+++ workarounds/stardewvalley.sh
@@ -4,10 +4,12 @@
 if [ -d "$extracted_scriptextender" ]; then
 	log_info "installing SMAPI in '$game_installation'"
 
+	game_installation_win=$(echo $game_installation | sed -e 's/^/Z:/' -e 's@/@\\@g')
+
 	# This command is ugly, but necessary 
 	# Needs to run through wineconsole in order to work with .NET's System.Console
 
-	"$utils/protontricks.sh" run-command "$game_appid" "wineconsole '${extracted_scriptextender}/SMAPI ${game_smapi_version} installer/internal/windows/SMAPI.Installer.exe' --install --no-prompt --game-path 'Z:\mnt\lingames\SteamLibrary\steamapps\common\Stardew Valley'" \
+	"$utils/protontricks.sh" run-command "$game_appid" "wineconsole '${extracted_scriptextender}/SMAPI ${game_smapi_version} installer/internal/windows/SMAPI.Installer.exe' --install --no-prompt --game-path '${game_installation_win}'" \
 		| "$dialog" loading "Installing SMAPI\nThis may take a while"
 
 	if [ "$?" != "0" ]; then
