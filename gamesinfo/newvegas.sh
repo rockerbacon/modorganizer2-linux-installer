8236a9d907209844c04d21c3d6af38d7ad1f57b2
Gamzee <lithiumturkin@protonmail.com>
2024-05-05 14:26:12 +0200

    Update NVSE

--- gamesinfo/newvegas.sh
+++ gamesinfo/newvegas.sh
@@ -3,6 +3,6 @@ game_nexusid="newvegas"
 game_appid=22380
 game_executable="FalloutNVLauncher.exe"
 game_protontricks=("d3dcompiler_43" "d3dx9")
-game_scriptextender_url="https://github.com/xNVSE/NVSE/releases/download/6.2.4/nvse_6_2_4.7z"
+game_scriptextender_url="https://github.com/xNVSE/NVSE/releases/download/6.3.5/nvse_6_3_5b.7z"
 game_scriptextender_files="*"
 
