4d8fc2c48f34293db4f38e4cd328d5d9c988d4e3
tdtooke <72276499+tdtooke@users.noreply.github.com>
2024-05-16 16:35:14 -0700

    Update starfield.sh
    Updated to latest SFSE

--- gamesinfo/starfield.sh
+++ gamesinfo/starfield.sh
@@ -3,9 +3,9 @@ game_nexusid="starfield"
 game_appid=1716740
 game_executable="Starfield.exe"
 game_protontricks=("xaudio2_7=native")
-game_scriptextender_url="https://sfse.silverlock.org/download/sfse_0_2_6.7z"
+game_scriptextender_url="https://sfse.silverlock.org/download/sfse_0_2_7.7z"
 game_scriptextender_files=( \
-	"sfse_0_2_6/sfse_1_10_32.dll" \
+	"sfse_0_2_6/sfse_1_11_36.dll" \
 	"sfse_0_2_6/sfse_loader.exe" \
 )
 
