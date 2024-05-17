46943e6bc54a618342287760897935060021d660
tdtooke <72276499+tdtooke@users.noreply.github.com>
2024-05-17 09:07:33 -0700

    Update starfield.sh
    Fixed folder having old name

--- gamesinfo/starfield.sh
+++ gamesinfo/starfield.sh
@@ -5,7 +5,7 @@ game_executable="Starfield.exe"
 game_protontricks=("xaudio2_7=native")
 game_scriptextender_url="https://sfse.silverlock.org/download/sfse_0_2_7.7z"
 game_scriptextender_files=( \
-	"sfse_0_2_6/sfse_1_11_36.dll" \
-	"sfse_0_2_6/sfse_loader.exe" \
+	"sfse_0_2_7/sfse_1_11_36.dll" \
+	"sfse_0_2_7/sfse_loader.exe" \
 )
 
