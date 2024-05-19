75d82d13f044825ad30197e93961af46ff6ab0f3
gmn42 <78309818+gmn42@users.noreply.github.com>
2024-05-19 03:46:10 -0700

    initialize variables in case no scriptextender is configured (#660)

--- step/download_external_resources.sh
+++ step/download_external_resources.sh
@@ -17,6 +17,9 @@ executable_winetricks="$shared/winetricks"
 downloaded_mo2="$downloads_cache/${mo2_url##*/}"
 extracted_mo2="${downloaded_mo2%.*}"
 
+downloaded_scriptextender=""
+extracted_scriptextender=""
+
 if [ -n "$game_scriptextender_url" ]; then
 	downloaded_scriptextender="$downloads_cache/${game_nexusid}_${game_scriptextender_url##*/}"
 	extracted_scriptextender="${downloaded_scriptextender%.*}"
