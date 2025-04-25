55652c2e4a75dd19d52eee87b1ebb431825efea5
Paradox <11278877+paradox-ng@users.noreply.github.com>
2025-04-26 01:09:15 +0200

    Update Starfield script extender
    Updating the Starfield script extender download package and extracted dll for the last update (as of today)

--- gamesinfo/starfield.sh
+++ gamesinfo/starfield.sh
@@ -3,9 +3,9 @@ game_nexusid="starfield"
 game_appid=1716740
 game_executable="Starfield.exe"
 game_protontricks=("xaudio2_7=native")
-game_scriptextender_url="https://sfse.silverlock.org/download/sfse_0_2_7.7z"
+game_scriptextender_url="https://sfse.silverlock.org/download/sfse_0_2_15.7z"
 game_scriptextender_files=( \
-	"sfse_0_2_7/sfse_1_11_36.dll" \
+	"sfse_0_2_7/sfse_1_14_70.dll" \
 	"sfse_0_2_7/sfse_loader.exe" \
 )
 
