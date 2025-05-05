dab11b0140e29daa9967ef208f33874795c4c6ab
cprin21 <107228497+cprin21@users.noreply.github.com>
2025-05-05 00:38:57 -0400

    Update cyberpunk.sh

--- gamesinfo/cyberpunk.sh
+++ gamesinfo/cyberpunk.sh
@@ -2,6 +2,6 @@ game_steam_subdirectory="Cyberpunk 2077"
 game_nexusid="cyberpunk2077"
 game_appid=1091500
 game_executable="REDprelauncher.exe"
-game_protontricks=("xaudio2_7=native" "d3dcompiler_47" "vcrun2022")
+game_protontricks=("xaudio2_7=native" "d3dcompiler_47" "vcrun2022" "arial" "fontsmooth=rgb")
 game_scriptextender_url=""
 game_scriptextender_files=""
