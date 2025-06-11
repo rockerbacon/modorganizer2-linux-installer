c9957c6df07916b4c04b54cdd6ba5581e197d903
Paradox <11278877+paradox-ng@users.noreply.github.com>
2025-06-11 18:26:14 +0200

    Update Oblivion Script Extender on oblivion.sh

--- gamesinfo/oblivion.sh
+++ gamesinfo/oblivion.sh
@@ -3,6 +3,6 @@ game_nexusid="oblivion"
 game_appid=22330
 game_executable="OblivionLauncher.exe"
 game_protontricks=("d3dcompiler_43" "d3dx9" "arial" "fontsmooth=rgb")
-game_scriptextender_url="https://github.com/llde/xOBSE/releases/download/22.5/xOBSE22.5.zip"
+game_scriptextender_url="https://github.com/llde/xOBSE/releases/download/22.12.1/xOBSE-22.12.1.zip"
 game_scriptextender_files="*"
 
