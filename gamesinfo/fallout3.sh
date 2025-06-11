bbaed297f27799f30c7cef7b3f53b92c45c38019
Paradox <11278877+paradox-ng@users.noreply.github.com>
2025-06-11 18:21:43 +0200

    Update Fallout 3 Script Extender on fallout3.sh

--- gamesinfo/fallout3.sh
+++ gamesinfo/fallout3.sh
@@ -3,6 +3,6 @@ game_executable="FalloutLauncherSteam.exe"
 game_nexusid="fallout3"
 game_steam_subdirectory="Fallout 3"
 game_protontricks=("d3dcompiler_43" "d3dx9" "arial" "fontsmooth=rgb")
-game_scriptextender_url="https://www.fose.silverlock.org/download/fose_v1_2_beta2.7z"
+game_scriptextender_url="https://www.fose.silverlock.org/beta/fose_v1_3_beta2.7z"
 game_scriptextender_files="*"
 
