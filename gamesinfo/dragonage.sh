fe450bf9200dbb1e05c128bf49dfdc7270d56118
furglitch <samsonger@proton.me>
2025-06-30 00:36:24 -0500

    Add base Dragon Age Origins support

---
+++ gamesinfo/dragonage.sh
@@ -0,0 +1,7 @@
+game_steam_subdirectory="Dragon Age Ultimate Edition"
+game_nexusid="dragonage"
+game_appid=17450
+game_executable="bin_ship\DAOrigins.exe"
+game_protontricks=("xaudio2_7=native" "d3dcompiler_43" "d3dx9" "arial" "fontsmooth=rgb")
+game_scriptextender_url=""
+game_scriptextender_files=""
