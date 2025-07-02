50c2032722e3777b217b924e24f3eca45caa207b
furglitch <samsonger@proton.me>
2025-07-02 12:19:06 -0500

    Switch DAO executable to launcher

--- gamesinfo/dragonage.sh
+++ gamesinfo/dragonage.sh
@@ -1,7 +1,7 @@
 game_steam_subdirectory="Dragon Age Ultimate Edition"
 game_nexusid="dragonage"
 game_appid=47810
-game_executable="bin_ship/DAOrigins.exe"
+game_executable="DAOriginsLauncher.exe"
 game_protontricks=("xaudio2_7=native" "d3dcompiler_43" "d3dx9" "arial" "fontsmooth=rgb")
 game_scriptextender_url=""
 game_scriptextender_files=""
