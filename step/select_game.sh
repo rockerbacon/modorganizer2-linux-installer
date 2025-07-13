16ab2a42b6b87526d0ef028cf52d1bca1e3f9138
Tim Elmer <tim@timelmer.com>
2025-07-12 20:01:18 -0700

    Add custom game option

--- step/select_game.sh
+++ step/select_game.sh
@@ -11,23 +11,32 @@ Which game would you like to manage with this installation?
 EOF
 )
 
+game_options=( \
+	"enderal" "Enderal: Forgotten Stories" \
+	"enderal_se" "Enderal: Forgotten Stories (Special Edition)" \
+	"fallout3" "Fallout 3" \
+	"fallout3_goty" "Fallout 3 - Game of the Year Edition" \
+	"fallout4" "Fallout 4" \
+	"newvegas" "Fallout: New Vegas" \
+	"newvegas_ru" "Fallout: New Vegas RU" \
+	"morrowind" "Morrowind" \
+	"oblivion" "Oblivion" \
+	"skyrim" "Skyrim" \
+	"skyrimspecialedition" "Skyrim Special Edition" \
+	"skyrimvr" "Skyrim VR" \
+	"starfield" "Starfield" \
+)
+
+# Excluded from list unless script is run with flag to reduce discoverability for the unfamiliar.
+if [ "$custom_game_enabled" == "1" ]; then
+	game_options+=("custom" "Custom Game")
+fi
+
 selected_game=$( \
 	"$dialog" \
 		radio \
 		450 "$screen_text" \
-		"enderal" "Enderal: Forgotten Stories" \
-		"enderal_se" "Enderal: Forgotten Stories (Special Edition)" \
-		"fallout3" "Fallout 3" \
-		"fallout3_goty" "Fallout 3 - Game of the Year Edition" \
-		"fallout4" "Fallout 4" \
-		"newvegas" "Fallout: New Vegas" \
-		"newvegas_ru" "Fallout: New Vegas RU" \
-		"morrowind" "Morrowind" \
-		"oblivion" "Oblivion" \
-		"skyrim" "Skyrim" \
-		"skyrimspecialedition" "Skyrim Special Edition" \
-		"skyrimvr" "Skyrim VR" \
-		"starfield" "Starfield"\
+		"${game_options[@]}"
 )
 
 if [ -z "$selected_game" ]; then
