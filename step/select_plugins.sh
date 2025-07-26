45e142230e031b2c27601d928830e30353c413c7
Furglitch <samsonger@proton.me>
2025-07-26 16:48:23 -0500

    Add 'Do you want plugins' prompt

--- step/select_plugins.sh
+++ step/select_plugins.sh
@@ -4,6 +4,24 @@ shopt -s nullglob
 
 screen_text=$( \
 cat << EOF
+Would you like to automatically install any MO2 plugins?
+EOF
+)
+
+button=$( 
+    "$dialog" \
+    question \
+    "$screen_text" \
+)
+
+if [ "$button" != 0 ]; then
+    log_info "user chose not to install plugins"
+    echo ""
+    return 0
+fi
+
+screen_text=$( \
+cat << EOF
 Which plugins would you like to install?
 EOF
 )
