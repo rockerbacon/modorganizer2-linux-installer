adf553af99f3384a7f0cb33039e3f75621771aef
Furglitch <samsonger@proton.me>
2025-07-27 16:06:07 -0500

    Update NV/OB script extenders

--- gamesinfo/newvegas_ru.sh
+++ gamesinfo/newvegas_ru.sh
@@ -3,6 +3,6 @@ game_nexusid="newvegas"
 game_appid=22490
 game_executable="FalloutNVLauncher.exe"
 game_protontricks=("d3dcompiler_43" "d3dx9" "vcrun2022")
-game_scriptextender_url="https://github.com/xNVSE/NVSE/releases/download/6.3.10/nvse_6_3_10b.7z"
+game_scriptextender_url="https://github.com/xNVSE/NVSE/releases/download/6.4.1/nvse_6_4_1.7z"
 game_scriptextender_files="*"
 
--- gamesinfo/oblivion.sh
+++ gamesinfo/oblivion.sh
@@ -3,6 +3,6 @@ game_nexusid="oblivion"
 game_appid=22330
 game_executable="OblivionLauncher.exe"
 game_protontricks=("d3dcompiler_43" "d3dx9")
-game_scriptextender_url="https://github.com/llde/xOBSE/releases/download/22.12.1/xOBSE-22.12.1.zip"
+game_scriptextender_url="https://github.com/llde/xOBSE/releases/download/22.13/xOBSE-22.13.zip"
 game_scriptextender_files="*"
 
