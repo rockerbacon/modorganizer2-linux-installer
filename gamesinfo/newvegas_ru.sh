073352be2dd8cb9c77abb05030076e1e9a00007e
Furglitch <samsonger@proton.me>
2025-07-26 23:33:16 -0500

    Add vcrun2022 to new vegas dlls

--- gamesinfo/newvegas_ru.sh
+++ gamesinfo/newvegas_ru.sh
@@ -2,7 +2,7 @@ game_steam_subdirectory="Fallout New Vegas enplczru"
 game_nexusid="newvegas"
 game_appid=22490
 game_executable="FalloutNVLauncher.exe"
-game_protontricks=("d3dcompiler_43" "d3dx9")
+game_protontricks=("d3dcompiler_43" "d3dx9" "vcrun2022")
 game_scriptextender_url="https://github.com/xNVSE/NVSE/releases/download/6.3.10/nvse_6_3_10b.7z"
 game_scriptextender_files="*"
 
