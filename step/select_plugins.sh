61e08a0783553d4bda3ab6059b1f7b4bd2430374
Furglitch <samsonger@proton.me>
2025-07-26 21:15:38 -0500

    Fix line indentation

--- step/select_plugins.sh
+++ step/select_plugins.sh
@@ -3,9 +3,9 @@
 shopt -s nullglob
 
 if ! command -v jq >/dev/null 2>&1; then
-    log_info "jq is not installed, skipping plugin selection"
-    echo ""
-    return 0
+	log_info "jq is not installed, skipping plugin selection"
+	echo ""
+	return 0
 fi
 
 screen_text=$( \
@@ -15,15 +15,15 @@ EOF
 )
 
 button=$( 
-    "$dialog" \
-    question \
-    "$screen_text" \
+	"$dialog" \
+	question \
+	"$screen_text" \
 )
 
 if [ "$button" != 0 ]; then
-    log_info "user chose not to install plugins"
-    echo ""
-    return 0
+	log_info "user chose not to install plugins"
+	echo ""
+	return 0
 fi
 
 screen_text=$( \
@@ -34,21 +34,21 @@ EOF
 
 array=()
 while IFS=$'\t' read -r id name; do
-    array+=("$id" "$name")
+	array+=("$id" "$name")
 done < <(jq -r '.[] | "\(.Identifier)\t\(.Name)"' "$pluginsinfo")
 
 
 selected_plugins=$( \
-    "$dialog" \
-        check \
-        450 "$screen_text" \
-        "${array[@]}" \
+	"$dialog" \
+		check \
+		450 "$screen_text" \
+		"${array[@]}" \
 )
 
 if [ -z "$selected_plugins" ]; then
-    log_info "no plugins selected"
-    echo ""
-    return 0
+	log_info "no plugins selected"
+	echo ""
+	return 0
 fi
 
 echo $selected_plugins
