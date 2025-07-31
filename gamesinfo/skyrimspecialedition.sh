ca0408efc6f4bcd1fb2ca9599d745bed04013101
Furglitch <samsonger@proton.me>
2025-07-31 01:11:12 -0500

    Fix d3d version, add xact for audio

--- gamesinfo/skyrimspecialedition.sh
+++ gamesinfo/skyrimspecialedition.sh
@@ -2,7 +2,7 @@ game_steam_subdirectory="Skyrim Special Edition"
 game_nexusid="skyrimspecialedition"
 game_appid=489830
 game_executable="SkyrimSELauncher.exe"
-game_protontricks=("xaudio2_7=native" "d3dcompiler_47" "vcrun2022")
+game_protontricks=("xaudio2_7=native" "xact" "d3dcompiler_43" "vcrun2022")
 game_scriptextender_url="https://skse.silverlock.org/beta/skse64_2_02_06.7z"
 game_scriptextender_files=( \
 	"skse64_2_02_06/Data" \
