7cd0658cfc1cbfdd6ac781d59a715267cc66c34e
Furglitch <78628049+Furglitch@users.noreply.github.com>
2025-07-31 11:32:32 -0500

    Add output logging for steam-redirector make

--- pack-release.sh
+++ pack-release.sh
@@ -26,7 +26,7 @@ release_files=( \
 
 original_workdir=$PWD
 cd "$script_root/steam-redirector" || exit 1
-make
+x86_64-w64-mingw32-gcc -v -municode -static -static-libgcc -Bstatic -lpthread -mwindows -o main.exe main.c win32_utils.c
 
 cd "$script_root" || exit 1
 tar -czvf "$script_root/mo2installer-$version.tar.gz" "${release_files[@]}"
