b7da407fad3653a3ce90b5f7b379a6cd3de73fab
Furglitch <samsonger@proton.me>
2025-08-10 11:48:42 -0500

    Add non-home info dialogue
    Resolves #812

--- step/select_install_dir.sh
+++ step/select_install_dir.sh
@@ -6,6 +6,15 @@ Select where you would like to install Mod Organizer 2
 EOF
 )
 
+screen_text=$( \
+cat << EOF
+The selected directory is outside of your /home directory.
+
+This should not cause issues, as long as you make sure Steam has permission to access this folder by setting the STEAM_COMPAT_MOUNTS argument in the game's properties.
+More information can be found in the Post-Installation instruction on GitHub.
+EOF
+)
+
 directory=$( \
 	"$dialog" \
 		directorypicker \
@@ -16,6 +25,14 @@ directory=$( \
 if [ -z "$directory" ]; then
 	log_error "no install directory selected"
 	exit 1
+fi
+
+if [[ "$directory" != $HOME/* ]]; then
+	button=$( \
+		"$dialog" \
+		infobox \
+		"$screen_text"
+	)
 fi
 
 echo "$directory"
